726f0620eaa337018c86910cd774789b647b3db4
andreidobrinski <andreiidobrinski@gmail.com>
2024-08-21 18:22:34 -0400

    add describe block to test suite

--- index.test.js
+++ index.test.js
@@ -1,53 +1,55 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { render } from 'cli-testing-library';
 import { resolve } from 'path';
 import { getBranchName } from './src/getBranchName.js';
 import { cleanupTests } from './cleanupTests.js';
 
-test('the CLI tool accepts args and returns the expected result', async () => {
-  const testProjectString = 'TESTPROJECT';
+describe('The e2e flow', () => {
+  test('the CLI tool accepts args and returns the expected result', async () => {
+    const testProjectString = 'TESTPROJECT';
 
-  const { findByText, userEvent } = await render('node', [
-    resolve(__dirname, `./index.js --project ${testProjectString}`),
-  ]);
+    const { findByText, userEvent } = await render('node', [
+      resolve(__dirname, `./index.js --project ${testProjectString}`),
+    ]);
 
-  // select branch type
-  const bugOption = await findByText('❯ Bug');
-  expect(bugOption).toBeTruthy();
-  // choose 'feature' and press enter
-  userEvent.keyboard('[ArrowDown][Enter]');
+    // select branch type
+    const bugOption = await findByText('❯ Bug');
+    expect(bugOption).toBeTruthy();
+    // choose 'feature' and press enter
+    userEvent.keyboard('[ArrowDown][Enter]');
 
-  // enter ticket number
-  const ticketInput = await findByText('Ticket number');
-  expect(ticketInput).toBeTruthy();
-  const ticketNumber = '123';
-  userEvent.keyboard(`${ticketNumber}[Enter]`);
+    // enter ticket number
+    const ticketInput = await findByText('Ticket number');
+    expect(ticketInput).toBeTruthy();
+    const ticketNumber = '123';
+    userEvent.keyboard(`${ticketNumber}[Enter]`);
 
-  // enter description
-  const descriptionInput = await findByText('Description');
-  expect(descriptionInput).toBeTruthy();
-  const description = 'description-test';
-  userEvent.keyboard(`${description}[Enter]`);
+    // enter description
+    const descriptionInput = await findByText('Description');
+    expect(descriptionInput).toBeTruthy();
+    const description = 'description-test';
+    userEvent.keyboard(`${description}[Enter]`);
 
-  // check that the branch name exists
-  const branchName = getBranchName({
-    type: 'feature',
-    project: testProjectString,
-    ticketNumber,
-    description,
+    // check that the branch name exists
+    const branchName = getBranchName({
+      type: 'feature',
+      project: testProjectString,
+      ticketNumber,
+      description,
+    });
+    const projectText = await findByText(branchName);
+
+    expect(projectText).toBeTruthy();
+
+    cleanupTests(branchName);
   });
-  const projectText = await findByText(branchName);
 
-  expect(projectText).toBeTruthy();
+  test('the CLI tool throws an error when no project is passed', async () => {
+    const { getByError } = await render('node', [
+      resolve(__dirname, `./index.js`),
+    ]);
 
-  cleanupTests(branchName);
-});
-
-test('the CLI tool throws an error when no project is passed', async () => {
-  const { getByError } = await render('node', [
-    resolve(__dirname, `./index.js`),
-  ]);
-
-  const errorText = getByError(`Error: must pass 'project' argument`);
-  expect(errorText).toBeTruthy();
+    const errorText = getByError(`Error: must pass 'project' argument`);
+    expect(errorText).toBeTruthy();
+  });
 });
